3c668fe2282535e864f95360f79f615b7e2ad5ee
prashmukho <prashantmukhopadhyay@gmail.com>
2016-02-25 11:27:53 +0530

    added typescript file index.ts to source directory for bundling and testing ts-loader is processing it via webpack; added html server response from node to test integration and script sourcing/generation of webpack bundle bundle.js

---
+++ app/index.ts
@@ -0,0 +1,2 @@
+const es6Test = 'ES6 in the house.';
+console.log(es6Test);
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var app = express();
 
+var PORT = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
-  res.send('Node says hello!');
+  res.send('\
+    <!DOCTYPE html>\
+    <html>\
+      <head>\
+        <title>Webpack Integration</title>\
+      </head>\
+      <body>\
+        <h1>Hello from webpack dev server!</h1>\
+        <script src="bundle.js"></script>\
+      </body>\
+    </html>\
+  ');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(PORT, function () {
+  console.log('Node listening on port:' + PORT);
 });
